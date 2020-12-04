490f5b03de36930f81b77facd32bbc87311c6d2e
Ashlyn Slawnyk <ashlyn.slawnyk@hudl.com>
2020-12-04 11:19:40 -0600

    2020: Day 4 solution, tests

--- years/2020/04/index.spec.ts
+++ years/2020/04/index.spec.ts
@@ -1,12 +1,280 @@
-import { p2020day4_part1, p2020day4_part2 } from '.';
+import {
+  isValidBirthYear,
+  isValidExpirationYear,
+  isValidEyeColor,
+  isValidHairColor,
+  isValidHeight,
+  isValidIssueYear,
+  isValidPassportId,
+  p2020day4_part1,
+  p2020day4_part2,
+} from '.';
 import { part1Tests, part2Tests } from './testCases';
 
 describe('2020 Day 4', () => {
-  describe('Part 1 Solution', () => {
-    it('has a test case so Jest works as expected', () => {
-      expect(true).toEqual(true);
+  describe('isValidBirthYear', () => {
+    it('Returns false for empty string', () => {
+      expect(isValidBirthYear('')).toEqual(false);
     });
 
+    it('Returns false for non-numerical input', () => {
+      expect(isValidBirthYear('abc')).toEqual(false);
+    });
+
+    it('Returns false for negative number', () => {
+      expect(isValidBirthYear('-1920')).toEqual(false);
+    });
+
+    it('Returns false for year before 1920', () => {
+      expect(isValidBirthYear('1919')).toEqual(false);
+    });
+
+    it('Returns false for year after 2002', () => {
+      expect(isValidBirthYear('2003')).toEqual(false);
+    });
+
+    it('Returns true for exactly 1920', () => {
+      expect(isValidBirthYear('1920')).toEqual(true);
+    });
+
+    it('Returns true for exactly 2002', () => {
+      expect(isValidBirthYear('2002')).toEqual(true);
+    });
+
+    it('Returns true for year between 1920 and 2002', () => {
+      expect(isValidBirthYear('1950')).toEqual(true);
+    });
+  });
+
+  describe('isValidIssueYear', () => {
+    it('Returns false for empty string', () => {
+      expect(isValidIssueYear('')).toEqual(false);
+    });
+
+    it('Returns false for non-numerical input', () => {
+      expect(isValidIssueYear('abc')).toEqual(false);
+    });
+
+    it('Returns false for negative number', () => {
+      expect(isValidIssueYear('-2010')).toEqual(false);
+    });
+
+    it('Returns false for year before 2010', () => {
+      expect(isValidIssueYear('2009')).toEqual(false);
+    });
+
+    it('Returns false for year after 2020', () => {
+      expect(isValidIssueYear('2021')).toEqual(false);
+    });
+
+    it('Returns true for exactly 2010', () => {
+      expect(isValidIssueYear('2010')).toEqual(true);
+    });
+
+    it('Returns true for exactly 2020', () => {
+      expect(isValidIssueYear('2020')).toEqual(true);
+    });
+
+    it('Returns true for year between 2010 and 2020', () => {
+      expect(isValidIssueYear('2015')).toEqual(true);
+    });
+  });
+
+  describe('isValidExpirationYear', () => {
+    it('Returns false for empty string', () => {
+      expect(isValidExpirationYear('')).toEqual(false);
+    });
+
+    it('Returns false for non-numerical input', () => {
+      expect(isValidExpirationYear('abc')).toEqual(false);
+    });
+
+    it('Returns false for negative number', () => {
+      expect(isValidExpirationYear('-2020')).toEqual(false);
+    });
+
+    it('Returns false for year before 2020', () => {
+      expect(isValidExpirationYear('2019')).toEqual(false);
+    });
+
+    it('Returns false for year after 2030', () => {
+      expect(isValidExpirationYear('2031')).toEqual(false);
+    });
+
+    it('Returns true for exactly 2020', () => {
+      expect(isValidExpirationYear('2020')).toEqual(true);
+    });
+
+    it('Returns true for exactly 2030', () => {
+      expect(isValidExpirationYear('2030')).toEqual(true);
+    });
+
+    it('Returns true for year between 2020 and 2030', () => {
+      expect(isValidExpirationYear('2025')).toEqual(true);
+    });
+  });
+
+  describe('isValidHeight', () => {
+    it('Returns false for anything that does not end in cm or in', () => {
+      expect(isValidHeight('150')).toEqual(false);
+    });
+
+    it('Returns false for non-numerical cm input', () => {
+      expect(isValidHeight('abccm')).toEqual(false);
+    });
+
+    it('Returns false for negative cm input', () => {
+      expect(isValidHeight('-150cm')).toEqual(false);
+    });
+
+    it('Returns false for cm input less than 150', () => {
+      expect(isValidHeight('149cm')).toEqual(false);
+    });
+
+    it('Returns false for cm input greater than 194', () => {
+      expect(isValidHeight('194cm')).toEqual(false);
+    });
+
+    it('Returns true for cm input exactly 150', () => {
+      expect(isValidHeight('150cm')).toEqual(true);
+    });
+
+    it('Returns true for cm input exactly 193', () => {
+      expect(isValidHeight('193cm')).toEqual(true);
+    });
+
+    it('Returns true for cm input between 150 and 193', () => {
+      expect(isValidHeight('175cm')).toEqual(true);
+    });
+
+    it('Returns false for non-numerical in input', () => {
+      expect(isValidHeight('abcin')).toEqual(false);
+    });
+
+    it('Returns false for negative in input', () => {
+      expect(isValidHeight('-59in')).toEqual(false);
+    });
+
+    it('Returns false for in input less than 59', () => {
+      expect(isValidHeight('58in')).toEqual(false);
+    });
+
+    it('Returns false for in input greater than 76', () => {
+      expect(isValidHeight('77in')).toEqual(false);
+    });
+
+    it('Returns true for in input exactly 59', () => {
+      expect(isValidHeight('59in')).toEqual(true);
+    });
+
+    it('Returns true for in input exactly 76', () => {
+      expect(isValidHeight('76in')).toEqual(true);
+    });
+
+    it('Returns true for in input between 59 and 76', () => {
+      expect(isValidHeight('65in')).toEqual(true);
+    });
+  });
+
+  describe('isValidHairColor', () => {
+    it('Returns false for empty string', () => {
+      expect(isValidHairColor('')).toEqual(false);
+    });
+
+    it('Returns false for anything that does not start with #', () => {
+      expect(isValidHairColor('abc123')).toEqual(false);
+    });
+
+    it('Returns false for non-hex inputs', () => {
+      expect(isValidHairColor('#ghijkl')).toEqual(false);
+    });
+
+    it('Returns false for hex inputs with fewer than 6 digits', () => {
+      expect(isValidHairColor('#abc12')).toEqual(false);
+    });
+
+    it('Returns false for hex inputs with more than 6 digits', () => {
+      expect(isValidHairColor('#abc1234')).toEqual(false);
+    });
+
+    it('Returns true for hex inputs with 6 digits', () => {
+      expect(isValidHairColor('#abc123')).toEqual(true);
+    });
+  });
+
+  describe('isValidEyeColor', () => {
+    it('Returns false for empty string', () => {
+      expect(isValidEyeColor('')).toEqual(false);
+    });
+
+    it('Returns false for invalid eye color', () => {
+      expect(isValidEyeColor('ylw')).toEqual(false);
+    });
+
+    it('Returns true for amb', () => {
+      expect(isValidEyeColor('amb')).toEqual(true);
+    });
+
+    it('Returns true for blu', () => {
+      expect(isValidEyeColor('blu')).toEqual(true);
+    });
+
+    it('Returns true for brn', () => {
+      expect(isValidEyeColor('brn')).toEqual(true);
+    });
+
+    it('Returns true for gry', () => {
+      expect(isValidEyeColor('gry')).toEqual(true);
+    });
+
+    it('Returns true for grn', () => {
+      expect(isValidEyeColor('grn')).toEqual(true);
+    });
+
+    it('Returns true for hzl', () => {
+      expect(isValidEyeColor('hzl')).toEqual(true);
+    });
+
+    it('Returns true for oth', () => {
+      expect(isValidEyeColor('oth')).toEqual(true);
+    });
+  });
+
+  describe('isValidPassportId', () => {
+    it('Returns false for empty string', () => {
+      expect(isValidPassportId('')).toEqual(false);
+    });
+
+    it('Returns false for non-numerical input', () => {
+      expect(isValidPassportId('abcdefghi')).toEqual(false);
+    });
+
+    it('Returns false for strings starting with non-numbers', () => {
+      expect(isValidPassportId('abc123456')).toEqual(false);
+    });
+
+    it('Returns false for strings ending with non-digits', () => {
+      expect(isValidPassportId('123456abc')).toEqual(false);
+    });
+
+    it('Returns false for strings with non-digits in the middle', () => {
+      expect(isValidPassportId('123abc456')).toEqual(false);
+    });
+
+    it('Returns false for strings with fewer than 9 digits', () => {
+      expect(isValidPassportId('12345678')).toEqual(false);
+    });
+
+    it('Returns false for strings with more than 9 digits', () => {
+      expect(isValidPassportId('1234567890')).toEqual(false);
+    });
+
+    it('Returns true for strings with exactly 9 digits', () => {
+      expect(isValidPassportId('123456789')).toEqual(true);
+    });
+  });
+
+  describe('Part 1 Solution', () => {
     part1Tests.forEach(testCase => {
       it(`${testCase.description}`, async () => {
         const actual = String(await p2020day4_part1(testCase.input));
@@ -16,10 +284,6 @@ describe('2020 Day 4', () => {
   });
 
   describe('Part 2 Solution', () => {
-    it('has a test case so Jest works as expected', () => {
-      expect(true).toEqual(true);
-    });
-
     part2Tests.forEach(testCase => {
       it(`${testCase.description}`, async () => {
         const actual = String(await p2020day4_part2(testCase.input));
--- years/2020/04/index.ts
+++ years/2020/04/index.ts
@@ -5,7 +5,7 @@ import * as LOGUTIL from '../../../util/log';
 import { performance } from 'perf_hooks';
 import { part1Tests, part2Tests } from './testCases';
 
-const { log, logSolution, trace } = LOGUTIL;
+const { log, logSolution } = LOGUTIL;
 
 const YEAR = 2020;
 const DAY = 4;
@@ -16,12 +16,78 @@ LOGUTIL.setDebug(DEBUG);
 // data path  : /Users/ashlyn.slawnyk/workspace/advent-of-code/years/2020/04/data.txt
 // problem url : https://adventofcode.com/2020/day/4
 
-export async function p2020day4_part1(input: string): Promise<string | undefined> {
-  return 'Not implemented';
+export const isValidBirthYear = (str: string): boolean => {
+  const year = parseInt(str);
+  return year >= 1920 && year <= 2002;
+};
+
+export const isValidIssueYear = (str: string): boolean => {
+  const year = parseInt(str);
+  return year >= 2010 && year <= 2020;
+};
+
+export const isValidExpirationYear = (str: string): boolean => {
+  const year = parseInt(str);
+  return year >= 2020 && year <= 2030;
+};
+
+export const isValidHeight = (str: string): boolean => {
+  const height = [...(/^([0-9]+)(cm|in)$/.exec(str) || [])];
+  if (!height.length) return false;
+
+  if (height[2] === 'cm') {
+    const heightCm = parseInt(height[1]);
+    return heightCm >= 150 && heightCm <= 193;
+  }
+  if (height[2] === 'in') {
+    const heightIn = parseInt(height[1]);
+    return heightIn >= 59 && heightIn <= 76;
+  }
+  return false;
+};
+
+export const isValidHairColor = (str: string): boolean => {
+  if (!str.startsWith('#')) return false;
+  const hexStr = str.substring(1);
+  return hexStr.length === 6 && !Number.isNaN(parseInt(hexStr, 16));
+};
+
+export const isValidEyeColor = (str: string): boolean => {
+  const allowedEyeColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
+  return allowedEyeColors.includes(str);
+};
+
+export const isValidPassportId = (str: string): boolean => /^[0-9]{9}$/.test(str);
+
+const passportRequiredFieldValidators = {
+  byr: isValidBirthYear,
+  iyr: isValidIssueYear,
+  eyr: isValidExpirationYear,
+  hgt: isValidHeight,
+  hcl: isValidHairColor,
+  ecl: isValidEyeColor,
+  pid: isValidPassportId,
+};
+
+const isValidPassport = (passport: string[][], validateValues = true): boolean => {
+  return Object.entries(passportRequiredFieldValidators).every(
+    ([field, validator]) =>
+      passport.findIndex(([key, value]) => key === field && (validateValues ? validator(value) : true)) > -1
+  );
+};
+
+const parseIntoPassportPairs = (input: string): string[][][] => {
+  return input.split('\n\n').map(str => str.split(/\s/g).map(kvp => kvp.split(':')));
+};
+
+export async function p2020day4_part1(input: string): Promise<number> {
+  const passportPairs = parseIntoPassportPairs(input);
+  return passportPairs.filter(p => isValidPassport(p, false)).length;
 }
 
-export async function p2020day4_part2(input: string): Promise<string | undefined> {
-  return 'Not implemented';
+export async function p2020day4_part2(input: string): Promise<number> {
+  const passportPairs = parseIntoPassportPairs(input);
+  return passportPairs.filter(p => isValidPassport(p)).length;
 }
 
 async function runTests() {
