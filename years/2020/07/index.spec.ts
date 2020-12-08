fd16d37e4726451d20e9061d0b2f571ab07988ab
Ashlyn Slawnyk <ashlyn.slawnyk@hudl.com>
2020-12-07 19:18:30 -0600

    2020: Day 7--fun with poorly formed graphs

--- years/2020/07/index.spec.ts
+++ years/2020/07/index.spec.ts
@@ -1,12 +1,80 @@
-import { p2020day7_part1, p2020day7_part2 } from '.';
+import { p2020day7_part1, p2020day7_part2, parseChildren, parseLine } from '.';
 import { part1Tests, part2Tests } from './testCases';
 
 describe('2020 Day 7', () => {
-  describe('Part 1 Solution', () => {
-    it('has a test case so Jest works as expected', () => {
-      expect(true).toEqual(true);
+  describe('parseChildren', () => {
+    it('Correctly parses no children', () => {
+      const input = 'contain no other bags.';
+      const result = parseChildren(input);
+
+      expect(result).toEqual([]);
     });
 
+    it('Correctly parses single children', () => {
+      const input = '1 shiny gold bag.';
+      const result = parseChildren(input);
+
+      expect(result).toEqual([{ name: 'shiny gold', count: 1 }]);
+    });
+
+    it('Correctly parses multiple children', () => {
+      const input = '3 posh turquoise bags, 4 dull lime bags, 3 vibrant fuchsia bags, 2 dull green bags.';
+      const result = parseChildren(input);
+
+      expect(result).toEqual([
+        { name: 'posh turquoise', count: 3 },
+        { name: 'dull lime', count: 4 },
+        { name: 'vibrant fuchsia', count: 3 },
+        { name: 'dull green', count: 2 },
+      ]);
+    });
+  });
+
+  describe('parseLine', () => {
+    it('Correctly parses no children', () => {
+      const parent = 'faded blue';
+      const input = `${parent} bags contain no other bags.`;
+      const result = parseLine(input);
+
+      expect(result).toEqual({ name: parent, count: 1, children: [] });
+    });
+
+    it('Correctly parses single children', () => {
+      const parent = 'bright white';
+      const input = `${parent} bags contain 1 shiny gold bag.`;
+      const result = parseLine(input);
+
+      expect(result).toEqual({
+        name: parent,
+        count: 1,
+        children: [
+          {
+            name: 'shiny gold',
+            count: 1,
+          },
+        ],
+      });
+    });
+
+    it('Correctly parses multiple children', () => {
+      const parent = 'posh purple';
+      const input = `${parent} bags contain 3 posh turquoise bags, 4 dull lime bags, 3 vibrant fuchsia bags, 2 dull green bags.`;
+      const result = parseLine(input);
+
+      expect(result).toEqual({
+        name: parent,
+        count: 1,
+        children: [
+          { name: 'posh turquoise', count: 3 },
+          { name: 'dull lime', count: 4 },
+          { name: 'vibrant fuchsia', count: 3 },
+          { name: 'dull green', count: 2 },
+        ],
+      });
+    });
+  });
+
+  describe('Part 1 Solution', () => {
     part1Tests.forEach(testCase => {
       it(`${testCase.description}`, async () => {
         const actual = String(await p2020day7_part1(testCase.input));
@@ -16,10 +84,6 @@ describe('2020 Day 7', () => {
   });
 
   describe('Part 2 Solution', () => {
-    it('has a test case so Jest works as expected', () => {
-      expect(true).toEqual(true);
-    });
-
     part2Tests.forEach(testCase => {
       it(`${testCase.description}`, async () => {
         const actual = String(await p2020day7_part2(testCase.input));
--- years/2020/07/index.ts
+++ years/2020/07/index.ts
@@ -5,7 +5,7 @@ import * as LOGUTIL from '../../../util/log';
 import { performance } from 'perf_hooks';
 import { part1Tests, part2Tests } from './testCases';
 
-const { log, logSolution, trace } = LOGUTIL;
+const { log, logSolution } = LOGUTIL;
 
 const YEAR = 2020;
 const DAY = 7;
@@ -16,12 +16,64 @@ LOGUTIL.setDebug(DEBUG);
 // data path  : /Users/ashlyn.slawnyk/workspace/advent-of-code/years/2020/07/data.txt
 // problem url : https://adventofcode.com/2020/day/7
 
-export async function p2020day7_part1(input: string): Promise<string | undefined> {
-  return 'Not implemented';
+type BagNode = {
+  name: string;
+  count: number;
+  children?: BagNode[];
+};
+
+const targetColor = 'shiny gold';
+const lineSplit = ' bags contain ';
+const noChildrenSubstr = 'no other bags.';
+const childrenPattern = /(\d+) ([a-z ]+) bags?[.,]?/g;
+
+export const parseChildren = (childrenStr: string): BagNode[] => {
+  if (childrenStr === noChildrenSubstr) return [];
+  return [...childrenStr.matchAll(childrenPattern)].map(([, count, color]) => {
+    return {
+      name: color,
+      count: parseInt(count),
+    };
+  });
+};
+
+export const parseLine = (line: string): BagNode => {
+  const parts = line.split(lineSplit);
+
+  return {
+    name: parts[0],
+    count: 1,
+    children: parseChildren(parts[1]),
+  };
+};
+
+const searchForParentsOf = (children: BagNode[], nodes: BagNode[]): BagNode[] => {
+  const directParents = nodes.filter(n => n.children?.some(child => children.map(c => c.name).includes(child.name)));
+  if (!directParents.length) return [];
+  return directParents.concat(searchForParentsOf(directParents, nodes));
+};
+
+const searchForChildrenOf = (parent: BagNode, nodes: BagNode[]): number => {
+  const initialNode = nodes.find(n => n.name === parent.name);
+  if (!initialNode) return 0;
+  if (!initialNode.children || !initialNode.children.length) {
+    return initialNode.count;
+  }
+
+  const children = initialNode.children.map(child => child.count * searchForChildrenOf(child, nodes));
+  return children.reduce((a, b) => a + b, 0) + initialNode.count;
+};
+
+export async function p2020day7_part1(input: string): Promise<number> {
+  const nodes = input.split('\n').map(parseLine);
+  const parents = searchForParentsOf([{ name: targetColor, count: 1 }], nodes);
+  return new Set(parents.map(p => p.name)).size;
 }
 
-export async function p2020day7_part2(input: string): Promise<string | undefined> {
-  return 'Not implemented';
+export async function p2020day7_part2(input: string): Promise<number> {
+  const nodes = input.split('\n').map(parseLine);
+  const children = searchForChildrenOf({ name: targetColor, count: 1 }, nodes);
+  return children - 1;
 }
 
 async function runTests() {
