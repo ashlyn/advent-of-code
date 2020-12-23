ba61b68fd6ed5225f61a62a4d5a9abe641fc2aaf
Ashlyn Slawnyk <ashlyn.slawnyk@hudl.com>
2020-12-23 00:07:23 -0600

    2002: Day 23 pt 1

--- years/2020/23/index.ts
+++ years/2020/23/index.ts
@@ -5,7 +5,7 @@ import * as LOGUTIL from '../../../util/log';
 import { performance } from 'perf_hooks';
 import { part1Tests, part2Tests } from './testCases';
 
-const { log, logSolution, trace } = LOGUTIL;
+const { log, logSolution } = LOGUTIL;
 
 const YEAR = 2020;
 const DAY = 23;
@@ -16,12 +16,52 @@ LOGUTIL.setDebug(DEBUG);
 // data path  : /Users/ashlyn.slawnyk/workspace/advent-of-code/years/2020/23/data.txt
 // problem url : https://adventofcode.com/2020/day/23
 
-export async function p2020day23_part1(input: string): Promise<string | undefined> {
-  return 'Not implemented';
+const playGame = (cups: number[], moves: number): number[] => {
+  const minValue = Math.min(...cups);
+  let index = 0;
+  for (let i = 0; i < moves; i++) {
+    // console.log(`-- move ${i + 1} --`);
+    // console.log(`cups: ${cups.map((c, i) => (i === index ? '(' + c + ')' : c)).join(' ')}`);
+    const currentCup = cups[index];
+    const threeCups = [1, 2, 3].map(j => cups[(index + j) % cups.length]);
+    // console.log(`pick up: ${threeCups.join(', ')}`);
+    cups = cups.filter(c => !threeCups.includes(c));
+    let destination = -1;
+    let target = currentCup - 1;
+    while (destination < 0) {
+      if (target < minValue) target = Math.max(...cups);
+      destination = cups.indexOf(target);
+      if (destination < 0) target--;
+    }
+    // console.log(`destination: ${cups[destination]} \n`);
+    cups.splice(destination + 1, 0, ...threeCups);
+    index = (cups.indexOf(currentCup) + 1) % cups.length;
+  }
+  const oneIndex = cups.indexOf(1);
+  return cups.map((c, i) => cups[(oneIndex + i + 1) % cups.length]);
+};
+
+const generateMoreCups = (cups: number[], n: number): number[] => {
+  const max = Math.max(...cups);
+  for (let i = max + 1; i <= n; i++) {
+    cups.push(i);
+  }
+  return cups;
+};
+
+export async function p2020day23_part1(input: string): Promise<string> {
+  const cups = input.split('').map(i => parseInt(i));
+  const ordered = playGame(cups, 100);
+  return ordered.slice(0, ordered.length - 1).join('');
 }
 
-export async function p2020day23_part2(input: string): Promise<string | undefined> {
-  return 'Not implemented';
+export async function p2020day23_part2(input: string): Promise<number> {
+  const cups = generateMoreCups(
+    input.split('').map(i => parseInt(i)),
+    1000000
+  );
+  const ordered = playGame(cups, 10000000);
+  return ordered[0] * ordered[1];
 }
 
 async function runTests() {
--- years/2020/23/testCases.ts
+++ years/2020/23/testCases.ts
@@ -1,2 +1,14 @@
-export const part1Tests: TestCase[] = [];
-export const part2Tests: TestCase[] = [];
+export const part1Tests: TestCase[] = [
+  {
+    description: 'AoC Input',
+    input: '389125467',
+    expected: '67384529',
+  },
+];
+export const part2Tests: TestCase[] = [
+  {
+    description: 'AoC Input',
+    input: '389125467',
+    expected: '149245887792',
+  },
+];
